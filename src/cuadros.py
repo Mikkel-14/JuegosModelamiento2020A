fe77eb3dd6be479cea3abfc13c3448d21fce91cb
Carolina Mendoza <carolina.mendoza@epn.edu.ec>
2020-08-18 17:27:49 -0500

    correccion cuadros

--- src/cuadros.py
+++ src/cuadros.py
@@ -1,51 +1,44 @@
 import pygame
 from abc import ABC, abstractmethod
+from posicion import *
+from listener import *
 
 pygame.init()
 
 
 class Cuadro(ABC):
-	def __init__(self, posicion):
-	    self.posicion = posicion
-	    super().__init__()
+    def __init__(self, posicion):
+        self.posicion = posicion
+        super().__init__()
 
-	@abstractmethod
-	def mover(self):
-		pass
-
-	@property
+    @property
     def posicion(self):
         return self.__posicion
 
-	@abstractmethod
-	def dibujar(self):
-		pass
+    @abstractmethod
+    def dibujar(self):
+        pass
+
+    @abstractmethod
+    def mover(self):
+        pass
+
 
 class Fondo(Cuadro):
-	    def __init__(self, imagen, posicion):
-	        self._Cuadro__posicion = posicion
-	        self.imagen = pygame.image.load(imagen)
-		def dibujar(self, ventana):
-	        ventana.blit(self.imagen, self.posicion.getPosicion())
-	 
-	    def mover(self):
-	        pass
+    def __init__(self, imagen, posicion):
+        self._Cuadro__posicion = posicion
+        self.imagen = pygame.image.load(imagen)
 
-class Camino(Cuadro):
-	    def __init__(self, imagen, posicion):
-	        self._Cuadro__posicion = posicion
-	        self.imagen = pygame.image.load(imagen)
-			
-		def dibujar(self, ventana):
-	        ventana.blit(self.imagen, self.posicion.getPosicion())
-	 
-	    def mover(self):
-	        pass
+    def dibujar(self, ventana):
+        ventana.blit(self.imagen, self.posicion.getPosicion())
+
+    def mover(self):
+        pass
 
 
 class Personaje(Cuadro):
-    def _init_(self, imagen, posicion):
-        self.Cuadro_posicion = posicion
+    def __init__(self, imagen, posicion):
+        self._Cuadro__posicion = posicion
         self.imagen = pygame.image.load(imagen)
 
     def dibujar(self, ventana):
@@ -54,33 +47,34 @@ class Personaje(Cuadro):
     def mover(self, dr, sl):
         keys = Listener.detectar()
         x, y = self.posicion.getPosicion()
-        if keys[pygame.K_UP] and sl.verificar((x,y-dr)):
+        if keys[pygame.K_UP] and sl.verificar((x, y-dr)):
             self.posicion.y -= dr
-        if keys[pygame.K_DOWN] and sl.verificar((x,y+dr)):
+        if keys[pygame.K_DOWN] and sl.verificar((x, y+dr)):
             self.posicion.y += dr
-        if keys[pygame.K_LEFT] and sl.verificar((x-dr,y)):
+        if keys[pygame.K_LEFT] and sl.verificar((x-dr, y)):
             self.posicion.x -= dr
         if keys[pygame.K_RIGHT] and sl.verificar((x+dr, y)):
             self.posicion.x += dr
 
+
 class MapaMuseo(Cuadro):
-    def init(self):
-        self._Cuadro__posicion = Posicion(0,0)
+    def __init__(self):
+        self._Cuadro__posicion = Posicion(0, 0)
         self.dictCuadros = dict()
         self.dictCuadros['camino'] = list()
-        self.dictCuadros['estaciones'] = list()
+        #self.dictCuadros['estaciones'] = list()
         self.dictCuadros['personaje'] = None
         self.dictCuadros['fondo'] = None
 
-	def agregarCuadros(self, cuadro):
-		if isinstance(cuadro, Camino):
-             self.dictCuadros['camino'].append(cuadro)
-        elif isinstance(cuadro, Estacion):
-             self.dictCuadros['estaciones'].append(cuadro)
+    def agregarCuadros(self, cuadro):
+        if isinstance(cuadro, Camino):
+            self.dictCuadros['camino'].append(cuadro)
+        # elif isinstance(cuadro, Estacion):
+        #     self.dictCuadros['estaciones'].append(cuadro)
         elif isinstance(cuadro, Personaje):
-             self.dictCuadros['personaje'] = cuadro
+            self.dictCuadros['personaje'] = cuadro
         elif isinstance(cuadro, Fondo):
-             self.dictCuadros['fondo'] = cuadro
+            self.dictCuadros['fondo'] = cuadro
 
     def accederLista(self):
         return self.dictCuadros
@@ -89,16 +83,27 @@ class MapaMuseo(Cuadro):
         self.dictCuadros['fondo'].dibujar(ventana)
         for camino in self.dictCuadros['camino']:
             camino.dibujar(ventana)
-        for estacion in self.dictCuadros['estaciones']:
-            estacion.dibujar(ventana)
+        # for estacion in self.dictCuadros['estaciones']:
+        #    estacion.dibujar(ventana)
         self.dictCuadros['personaje'].dibujar(ventana)
         pygame.display.update()
-
 
     def mover(self, solapamiento):
         self.dictCuadros['fondo'].mover()
         for camino in self.dictCuadros['camino']:
             camino.mover()
-        for estacion in self.dictCuadros['estaciones']:
-            estacion.mover()
+        # for estacion in self.dictCuadros['estaciones']:
+        #    estacion.mover()
         self.dictCuadros['personaje'].mover(34, solapamiento)
+
+
+class Camino(Cuadro):
+    def __init__(self, imagen, posicion):
+        self._Cuadro__posicion = posicion
+        self.imagen = pygame.image.load(imagen)
+
+    def dibujar(self, ventana):
+        ventana.blit(self.imagen, self.posicion.getPosicion())
+
+    def mover(self):
+        pass
