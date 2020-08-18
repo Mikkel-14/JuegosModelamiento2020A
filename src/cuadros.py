26a3497c872773a7929ffaa5b02cfdd685713d3e
alejandrollanganate <luis.llanganate@epn.edu.ec>
2020-08-17 20:26:45 -0500

    Creación MapaMuseo

--- src/cuadros.py
+++ src/cuadros.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 
 pygame.init()
 
+
 class Cuadro(ABC):
 	def __init__(self, posicion):
 	    self.posicion = posicion
@@ -24,4 +25,12 @@ class Fondo(Cuadro):
 	    def __init__(self, imagen, posicion):
 	        self._Cuadro__posicion = posicion
 	        self.imagen = pygame.image.load(imagen)
-			
+
+class MapaMuseo(Cuadro):
+    def init(self):
+        self._Cuadro__posicion = Posicion(0,0)
+        self.dictCuadros = dict()
+        self.dictCuadros['camino'] = list()
+        self.dictCuadros['estaciones'] = list()
+        self.dictCuadros['personaje'] = None
+        self.dictCuadros['fondo'] = None
