9e7148dce043ef8185e4b11a406b0cddf1e150e7
Santiago2693 <69229850+Santiago2693@users.noreply.github.com>
2020-08-19 22:24:29 -0500

    Actualizacion carpeta assets.

--- src/assets/herramientas.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-# Este método permite obtener una dirección absoluta de un fichero o archivo
-def obtenerPathAbsoluto(pathRelativo):
-    pathAbsolutoScript = os.path.dirname(__file__)
-    pathAbsoluto = os.path.join(pathAbsolutoScript, pathRelativo)
-    return pathAbsoluto
