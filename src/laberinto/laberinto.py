c6725a48be0e0f9c9e54d69847f32cd7011c89dd
Alex970705 <59580273+Alex970705@users.noreply.github.com>
2020-08-19 22:21:43 -0500

    adicion metodos clase laberinto

--- src/laberinto/laberinto.py
+++ src/laberinto/laberinto.py
@@ -12,3 +12,22 @@ class Laberinto(Juego):
         self.ventana = None
         self.imagen = pygame.image.load(os.path.join(os.path.dirname(__file__),'img/inicioLaberinto.png'))
         self.clock = pygame.time.Clock()
+    
+    def iniciarJuego(self):
+        self.ventana = pygame.display.set_mode(self.dimensiones)
+        pygame.display.set_caption(self.titulo)
+        bandera=True
+        while bandera:
+            self.clock.tick(30)
+            self.ventana.blit(self.imagen, (0,0))
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    bandera=False
+                    pygame.quit()
+
+    def reiniciarJuego(self):
+        pass
+
+    def salirJuego(self):
+        pass
