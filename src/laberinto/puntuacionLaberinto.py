d1f538d18c58c37a61684b89903272bf89a13e6f
IIDinkaII <diana.lopez03@epn.edu.ec>
2020-09-09 19:52:03 -0500

    Corrección Solapamiento, Cambios en Puntuación y Archivos
    Adaptación del método verificar a lista de cuadros anónima. Métodos actualizar y calcular puntos en Puntuación. Cambios en los archivos para el laberinto.

--- src/laberinto/puntuacionLaberinto.py
+++ src/laberinto/puntuacionLaberinto.py
@@ -1,10 +1,14 @@
-class PuntuacionLaberinto():
+from laberinto.observadorLaberinto import *
+import laberinto.settingsLaberinto as s
+
+class PuntuacionLaberinto(ObservadorLaberinto):
     def __init__(self):
         self.numeroErrores = 0
         self.puntos =0
 
-    def actualizarNumErrores(self):
-        self.numeroErrores += 1
+    def actualizar(self, virus, enemigo, meta, perdida, corazon):
+        if virus or enemigo:
+            self.numeroErrores += 1
 
-    def calculoPuntos(self):
-        pass
+    def calcularPuntos(self):
+        self.puntos = int(s.puntosTOTALES - (self.numeroErrores * s.puntosTOTALES / 4))
