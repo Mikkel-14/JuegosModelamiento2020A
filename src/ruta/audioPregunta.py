0cd557eb4ad91b4a1b5297cf45f2239687470570
alejandrollanganate <luis.llanganate@epn.edu.ec>
2020-09-10 00:57:14 -0500

    Comentarios en AudioPregunta y Playlist

--- src/ruta/audioPregunta.py
+++ src/ruta/audioPregunta.py
@@ -1,32 +1,49 @@
+#=============================================================================================
+#                               JUEGO RUTA MAYA - Version 0.9
+#                              Clase AudioPregunta y Playlist
+# Implementado por: Alejandro Llanganate, Anderson Cárdenas, Henrry Cordovillo y David Moreno
+#=============================================================================================
+
 import pygame
-from time import time
 from ruta.assets.herramientas import *
 from ruta.figuras import *
 
 
 class AudioPregunta:
     def __init__(self, audio, letraRespuesta):
-        pygame.mixer.init()
-        self.audio = pygame.mixer.Sound(obtenerPathAbsoluto(audio, __file__))
+        self.audio = pygame.mixer.Sound(obtenerPathAbsoluto(audio, __file__)) # carga el audio unicamente en formato wav
         self.letraRespuesta = letraRespuesta
         self.estadoReproducion = True
 
-    def reproducir(self, camino, opciones):
-        keys = pygame.key.get_pressed()
-        if(keys[pygame.K_s] and camino.notificar() == False):
-            self.estadoReproducion = False
+    def reproducir(self, movimientoCamino, opciones):
+        keys = pygame.key.get_pressed() # captura si se aplastó una tecla
         
-        if(self.estadoReproducion and camino.notificar() == False):
-            self.audio.play()
-
-        if(self.estadoReproducion == False and camino.notificar() == False ):
-            pygame.mixer.stop()
+        if keys[pygame.K_s] and movimientoCamino == False: # si la tecla es la "S" y el camino está detenido
+            self.estadoReproducion = False 
+            pygame.mixer.stop() # se detiene el audio completamente
             for opcion in opciones:
                 if opcion.obtenerVisibilidad() == False:
-                    opcion.setVisibilidad(True)
+                    opcion.setVisibilidad(True) # se hacen visibles las Figuras Opción
+        elif self.estadoReproducion and movimientoCamino == False:  
+            self.audio.play() # se reproduce el audio si el caminon está detenido y si el estado de reproducción es True
 
     def setEstadoReproducido(self, value):
         self.estadoReproducion = value
     
     def obtenerLetraRespuesta(self):
         return self.letraRespuesta
+
+
+class Playlist:
+
+    listaAudiosPreguntas = []
+
+    def añadirAudioPregunta(self, pregunta):
+        if isinstance(pregunta, AudioPregunta):
+            self.listaAudiosPreguntas.append(pregunta)
+
+    def quitarAudioPregunta(self, pregunta):
+        self.listaAudiosPreguntas.remove(pregunta)
+    
+    def obtenerAudiosPreguntas(self):
+        return self.listaAudiosPreguntas
