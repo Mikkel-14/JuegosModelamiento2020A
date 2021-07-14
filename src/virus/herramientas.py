fe79ddea33349752a081ae1f8b79c1ffad317f59
Miguel Muñoz <67567150+Mikkel-14@users.noreply.github.com>
2021-07-14 09:31:12 -0500

    Update herramientas.py

--- src/virus/herramientas.py
+++ src/virus/herramientas.py
@@ -1,7 +1,7 @@
 import os
 
 
-# Este método permite obtener una dirección absoluta de un fichero o archivo
+# Este método permite obtener una dirección absoluta de un fichero o archivo.
 def obtenerPathAbsoluto(pathRelativo):
     pathAbsolutoScript = os.path.dirname(__file__)
     pathAbsoluto = os.path.join(pathAbsolutoScript, pathRelativo)
